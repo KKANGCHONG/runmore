aab87c34a195e8160053d5a985729e676fbbd3c6
KKANGCHONG <leee0414@naver.com>
2026-01-13 06:15:04 +0900

    style(splash): make splash image cover full viewport on web and native

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, StyleSheet, Platform } from 'react-native';
+import { View, Image, StyleSheet, Platform, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 export default function SplashScreen() {
   return (
@@ -7,7 +9,7 @@ export default function SplashScreen() {
       <Image
         source={require('../../assets/splash.png')}
         style={styles.image}
-        resizeMode="contain"
+        resizeMode="cover"
       />
     </View>
   );
@@ -17,16 +19,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
     ...(Platform.OS === 'web' && {
       position: 'fixed',
       top: 0,
       left: 0,
       right: 0,
       bottom: 0,
-      width: '100%',
-      height: '100%',
+      width: '100vw',
+      height: '100vh',
       zIndex: 9999,
     }),
   },
@@ -34,7 +36,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     ...(Platform.OS === 'web' && {
-      objectFit: 'contain',
+      width: '100vw',
+      height: '100vh',
+      objectFit: 'cover',
     }),
   },
 });
